86f313d1a5a9766b2fdf834343de56ad114f07bb
ricardoferreira-spec <ricardoferreirafatec@yahoo.com.br>
2025-01-14 09:16:54 -0300

    2025-01-14
    tratamentos lista de compras

---
+++ script/script.js
@@ -0,0 +1,86 @@
+let listaDeCompras = []
+
+function adicionarItem(){
+    let itemInput = document.getElementById("item")
+    let quantidadeInput = document.getElementById("quantidade")
+
+    if(itemInput.value.trim() === ""){
+        return 
+        /// quando a pessoa não preencher item não preeche nada.
+        /// trim quando a pessoa  preencher espaço em branco não preeche nada.
+    }
+ 
+    let novoItem = {
+        id: Date.now(),
+        nome: itemInput.value,
+        quantidade: quantidadeInput.value || 1,
+        comprado: false
+    }
+
+    /// || 1 = quando não preecher a quantidade já adiciona 1 na quantidade
+
+    listaDeCompras.push(novoItem)
+    
+    atualizarInterface()
+    itemInput.value = ""            /// para apagar o imput quando eu digitar um novo item
+    quantidadeInput.value = ""      /// ficava histórico na combo
+    itemInput.focus()               /// cursor volta para caixa de item para digitar um novo item
+}
+
+
+function removerItem(id) {
+    listaDeCompras = listaDeCompras.filter((item) => item.id != id)
+    atualizarInterface()
+}
+
+function limparLista() {
+    listaDeCompras = []
+    atualizarInterface()
+}
+
+function atualizarInterface(){
+    let lista = document.querySelector(".lista")
+    lista.innerHTML = ""
+
+    for(let i = 0; i < listaDeCompras.length; i++) {
+        let item = document.createElement("li")
+        item.innerHTML = `
+                <input type='checkbox' onchange = "toggleItem(${listaDeCompras[i].id})" ${listaDeCompras[i].comprado ? "checked" : ""}/> 
+                <p>${listaDeCompras[i].nome} x ${listaDeCompras[i].quantidade}</p>
+                <button onclick="removerItem(${listaDeCompras[i].id})">X</button>
+        `
+        lista.append(item)
+    }
+}
+
+//// o  "checked" : "" / falso soluciona o bug de quando adiciona item com algum selecionado não apaga mais o selecionado
+
+function toggleItem(id) {
+    const item = listaDeCompras.find((item) => item.id == id)
+    item.comprado = !item.comprado
+    console.log(item)
+}
+
+function limparComprados(){
+    listaDeCompras = listaDeCompras.filter((item) => item.comprado != true)
+    atualizarInterface()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  listaDeCompras = listaDeCompras.filter((item) => item.comprado != true)
